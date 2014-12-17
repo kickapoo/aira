cf9276dafe8c629f1c1b3301195157945db27581
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2014-12-17 20:09:34 +0200

    Updated templates / Add BS3 forms

--- aira_project/urls.py
+++ aira_project/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-# from aira.views import edit_profile, home, index, user_map, \
-#     update_fields, update_crop, update_irrigationlog, next_irrigation
 
 urlpatterns = patterns(
     '',
@@ -23,15 +21,4 @@ urlpatterns = patterns(
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'', include('aira.urls')),
-    # url(r'^$', index, name="welcome"),
-    # url(r'^home/$', home, name="home"),
-    # url(r'^user_map/', user_map, name="user_map"),
-    # url(r'^edit_profile', edit_profile, name="edit_profile"),
-    # url(r'^update_fields', update_fields, name="update_fields"),
-    # url(r'^update_crop/(?P<field_id>\d+)$', update_crop,
-    #     name="update_crop"),
-    # url(r'^update_irrigationlog/(?P<crop_id>\d+)$', update_irrigationlog,
-    #     name="update_irrigationlog"),
-    # url(r'^update_irrigationlog/(?P<field_id>\d+)/(?P<crop_id>\d+)$',
-    #     next_irrigation, name="next_irrigation"),
 )
