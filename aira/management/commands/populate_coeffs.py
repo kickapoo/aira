558c809dad881ce617d3ba329442f31153e96949
Vagrant User <vagrant@aira.dev>
2014-12-30 18:57:39 +0000

    Add management command 'populate_coeffs' (fixes #13)

---
+++ aira/management/commands/populate_coeffs.py
@@ -0,0 +1,37 @@
+import os
+import csv
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from aira.models import CropType, IrrigationType
+
+
+class Command(BaseCommand):
+    help = """Populate CropType and IrrigationType tables
+	      of aira database with essential constant coefficients"""
+
+    def handle(self, *args, **options):
+        try:
+            croptype_csv = os.path.join(settings.AIRA_PARAMETERS_FILE_DIR,
+                                        'croptype.csv')
+            irrt_csv = os.path.join(settings.AIRA_PARAMETERS_FILE_DIR,
+                                    'irr_eff.csv')
+
+            with open(irrt_csv) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    _, created = IrrigationType.objects.get_or_create(
+									irrt_name=row[0],
+                                                                      	irrt_eff=float(row[1])
+                                                                      )
+            with open(croptype_csv) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    _, created = CropType.objects.get_or_create(
+								ct_name=row[0],
+                                                                ct_coeff=float(row[1]),
+                                                                ct_rd=float(row[2])
+                                                                )
+        except:
+            raise CommandError("Use 'makemigrations aira' to create aira tables")
+
+        self.stdout.write('Aira database is successfully updated')
