65e8bab18dad9c1c66dcb42aabd7ad065990286e
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2015-01-19 10:54:27 +0200

    Fix PEP8 issues

--- aira/management/commands/populate_coeffs.py
+++ aira/management/commands/populate_coeffs.py
@@ -7,7 +7,7 @@ from aira.models import CropType, IrrigationType
 
 class Command(BaseCommand):
     help = """Populate CropType and IrrigationType tables
-	      of aira database with essential constant coefficients"""
+              of aira database with essential constant coefficients"""
 
     def handle(self, *args, **options):
         try:
@@ -19,18 +19,14 @@ class Command(BaseCommand):
             with open(irrt_csv) as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    _, created = IrrigationType.objects.get_or_create(
-									irrt_name=row[0],
-                                                                      	irrt_eff=float(row[1])
-                                                                      )
+                    _, created = IrrigationType.objects.get_or_create(irrt_name=row[0],
+                                                                      irrt_eff=float(row[1]))
             with open(croptype_csv) as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    _, created = CropType.objects.get_or_create(
-								ct_name=row[0],
+                    _, created = CropType.objects.get_or_create(ct_name=row[0],
                                                                 ct_coeff=float(row[1]),
-                                                                ct_rd=float(row[2])
-                                                                )
+                                                                ct_rd=float(row[2]))
         except:
             raise CommandError("Use 'makemigrations aira' to create aira tables")
 
