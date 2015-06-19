8478e501767b1fdf71e3ccc7191f34e632666f8a
Antonis Christofides <anthony@itia.ntua.gr>
2015-06-19 16:17:06 +0300

    Even more forgiving temporary solution for #97

--- aira/management/commands/runswb.py
+++ aira/management/commands/runswb.py
@@ -12,12 +12,19 @@ class Command(BaseCommand):
         daily_r_fps, daily_e_fps, hourly_r_fps, hourly_e_fps = \
             load_meteodata_file_paths()
         for agrifield in Agrifield.objects.all():
-            model_run(agrifield, 'NO', daily_r_fps, daily_e_fps, hourly_r_fps,
-                      hourly_e_fps, ignore_cache=True)
-            model_run(agrifield, 'YES', daily_r_fps, daily_e_fps, hourly_r_fps,
-                      hourly_e_fps, ignore_cache=True)
-            # The following try-except is an emergency solution for the system
-            # to continue to work despite issue #97. Remove after fixing #97.
+            # The following try-excepts are an emergency solution for the
+            # system to continue to work despite issue #97. Remove after fixing
+            # #97.
+            try:
+                model_run(agrifield, 'NO', daily_r_fps, daily_e_fps, hourly_r_fps,
+                          hourly_e_fps, ignore_cache=True)
+            except:
+                pass
+            try:
+                model_run(agrifield, 'YES', daily_r_fps, daily_e_fps, hourly_r_fps,
+                          hourly_e_fps, ignore_cache=True)
+            except:
+                pass
             try:
                 performance_chart(agrifield, daily_r_fps, daily_e_fps,
                                   ignore_cache=True)
