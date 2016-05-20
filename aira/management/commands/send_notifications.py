7d00ebe2d87aaad4464c15349baab7bb36bb8430
Antonis Christofides <anthony@itia.ntua.gr>
2016-05-20 15:08:34 +0300

    Simplify send_notifications code

--- aira/management/commands/send_notifications.py
+++ aira/management/commands/send_notifications.py
@@ -1,21 +1,19 @@
+from __future__ import unicode_literals
+
 from datetime import date, datetime
 
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
 from django.template import Context
-from django.template.loader import get_template
-from django.utils.translation import ugettext_lazy as _
+from django.template.loader import get_template, render_to_string
 from django.utils import translation
+from django.utils.translation import ugettext_lazy as _
 
-from django.contrib.auth.models import User
-from aira.models import Profile, Agrifield
-from aira.models import notification_options
-from aira.irma.main import email_users_response_data, agripoint_in_raster
-
-from django.contrib.sites.models import Site
-
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
+from aira.irma.main import agripoint_in_raster, email_users_response_data
+from aira.models import notification_options, Profile
 
 
 class Command(BaseCommand):
@@ -24,24 +22,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.template = get_template('aira/email_notification.html')
         for user in User.objects.all():
-            if self.send_notification(user):
-                if Agrifield.objects.filter(owner=user).exists():
-                    user_agrifields = Agrifield.objects.filter(owner=user)
-                    self.notify_user(user, user_agrifields, user)
-                if Profile.objects.filter(supervisor=user).exists():
-                    for supervised_user in Profile.objects.filter(supervisor=user):
-                        supervised_user = User.objects.get(username=supervised_user.farmer)
-                        if Agrifield.objects.filter(owner=supervised_user).exists():
-                            supervised_agrifields = Agrifield.objects.filter(owner=supervised_user)
-                            self.notify_user(user, supervised_agrifields, supervised_user)
+            if not self.must_send_notification(user):
+                continue
 
-    def send_notification(self, user):
-        if Profile.objects.filter(farmer=user).exclude(notification='').exists():
-            user_profile = Profile.objects.get(farmer=user)
-            return notification_options[user_profile.notification][1](date.today())
-        return False
+            # Send notification for user's own agrifields
+            self.notify_user(user, user.agrifield_set.all(), user)
 
-    def get_email_context(self, agrifields, user, owner ):
+            # If user is a supervisor, send an additional notification to him
+            # for each of the supervised users.
+            for supervised_user in User.objects.filter(profile__supervisor=user
+                                                       ):
+                self.notify_user(user, supervised_user.agrifield_set.all(),
+                                 supervised_user)
+
+    def must_send_notification(self, user):
+        try:
+            return notification_options[user.profile.notification][1](
+                date.today())
+        except (Profile.DoesNotExist, KeyError):
+            return False
+
+    def get_email_context(self, agrifields, user, owner):
         context = Context()
         for f in agrifields:
             f.results = email_users_response_data(f)
@@ -55,15 +56,15 @@ class Command(BaseCommand):
         return context
 
     def notify_user(self, user, agrifields, owner):
-        # Clear agrifields from the ones outside the study_area
-        translation.activate(user.profile.email_language)
         agrifields = [f for f in agrifields if agripoint_in_raster(f)]
-        if len(agrifields) > 0:
-            msg_html = render_to_string('aira/email_notification.html',
-                                         self.get_email_context(agrifields,
-                                                                user, owner))
-            send_mail(_(u"Irrigation status for ") + unicode(owner),
-                      '',
-                      settings.DEFAULT_FROM_EMAIL,
-                      [user.email, ],
-                      html_message=msg_html)
+        if not agrifields:
+            return
+        translation.activate(user.profile.email_language)
+        msg_html = render_to_string('aira/email_notification.html',
+                                    self.get_email_context(agrifields,
+                                                           user, owner))
+        send_mail(_("Irrigation status for ") + unicode(owner),
+                  '',
+                  settings.DEFAULT_FROM_EMAIL,
+                  [user.email, ],
+                  html_message=msg_html)
