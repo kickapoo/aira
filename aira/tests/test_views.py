kickapoo/aira

commit e02225eb534625144da5a3711e8017ce0ba859e1

Remove unused templates and move small templates (part of #211)

aira/views.py
@@ -23,7 +23,7 @@ from .models import Agrifield, IrrigationLog, Profile
 
 
 class IrrigationPerformanceView(TemplateView):
-    template_name = "aira/performance-chart.html"
+    template_name = "aira/performance_chart/main.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -301,7 +301,7 @@ class CreateIrrigationLogView(CreateView):
 class UpdateIrrigationLogView(UpdateView):
     model = IrrigationLog
     form_class = IrrigationlogForm
-    template_name = "aira/irrigationlog_update.html"
+    template_name = "aira/irrigationlog_update/main.html"
 
     def get_success_url(self):
         field = Agrifield.objects.get(pk=self.kwargs["pk_a"])

aira/templates/aira/irrigationlog_confirm_delete.html
@@ -1,25 +1,0 @@
-{% extends 'aira/base/main.html' %}
-{% load i18n %}
-
-{% load bootstrap3 %}
-
-{% block title %} {{ user }} {% endblock %}
-
-{% block content %}
-<div class="container">
-  <div class="row">
-    <div class="col-md-12 col-xs-offset-1">
-      <h3>{% trans "Are you sure you want to delete" %} "<a href="{% url 'update_irrlog' object.agrifield.id object.id %}">{{ object.time}}</a>" {% trans "Irrigation Event" %} {% trans "?" %}</h3>
-      <br>
-      <form method="post" role="form" class="form-horizontal">
-        {% csrf_token %}
-        {% buttons %}
-        <div class="col-sm-offset-4">
-          <button type="submit" class="btn btn-danger">{% trans "Delete" %}</button>
-          <br>
-        </div>
-        {% endbuttons %}
-      </form>
-    </div>
-</div>
-{% endblock %}

aira/templates/aira/irrigationlog_form.html
@@ -1,91 +1,0 @@
-{% extends 'aira/base/main.html' %}
-{% load i18n %}
-
-{% load bootstrap3 %}
-
-{% block title %} {% trans "Irrigation Log" %} {% endblock %}
-
-{% block content %}
-{% if user.username == "demo" %}
- <div class="alert alert-info">
-    <a href="#" class="close" data-dismiss="alert">&times;</a>
-    <i class="fa fa-info-circle fa-2x"></i>&nbsp; <span style="font-size:32px">{% trans "demo" %}</span>
-</div>
-{% endif %}
-{% if logs %}
-  <div class="alert alert-warning">
-    <a href="#" class="close" data-dismiss="alert">&times;</a>
-    <i class="fa fa-hand-o-right fa-1x"></i>&nbsp;
-    {% trans "You have" %} <b><span class="text text-success">{{logs_count}}</span></b> {% trans "Irrigation Events in your database" %}.
-  </div>
-{% endif %}
-<hr>
-
-<div class="container">
-  <div class="row">
-  <h4>{% trans "Add Irrigation Event" %}</h4>
-  <div class="col-md-4">
-    <form method="post" role="form" class="form-horizontal">
-      {% csrf_token %}
-      {% bootstrap_form form %}
-      {% buttons %}
-        <span class="add-on"><i class="icon-th"></i></span>
-        {% if user.username != "demo" %}
-        <button type="submit" class="btn btn-success btn-xs">{% trans "Add" %}</button>
-        <a class="btn btn-success btn-xs" href="{% url 'home' agrifield_user %}"> {% trans "Back" %} </a>
-
-        {% endif %}
-      {% endbuttons %}
-    </form>
-  </div>
-  <div class="col-md-1">
-  </div>
-  {% if logs %}
-  <div class="col-md-7">
-    <div class="table-responsive">
-      <table class="table">
-        <h4> {% trans "List of" %} {% trans "Irrigation Events" %}, <a href="{% url 'update_agrifield' agrifield.id %}">{{ agrifield.name }}</a></h4>
-        <hr>
-        <thead>
-          <tr>
-            <th>{% trans "#id" %}</th>
-            <th>{% trans "Datetime" %}</th>
-            <th>{% trans "Applied water" %} (m³)</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {% for l in logs  %}
-            <tr>
-              <td>{{ l.id }}</td>
-              <td><a href="{% url 'update_irrlog' agrifield.id l.id %}">{{l.time}}</a></td>
-              <td align="center">{{l.applied_water|floatformat:"2"}}</td>
-              {% if user.username != "demo" %}
-              <td><a href="{% url 'delete_irrlog' agrifield.id l.id %}">{% trans "Delete" %} </a></td>
-              {% else %}
-              <td -</td>
-              {% endif %}
-            </tr>
-          {% endfor %}
-        </tbody>
-      </table>
-    </div>
-  </div>
-  {% endif %}
-  </div> <!-- row -->
-</div> <!-- container -->
-{% endblock %}
-
-{% block extrajs %}
-      <script type="text/javascript">
-          $("#id_time").datetimepicker({
-            format: "yyyy-mm-dd hh:ii",
-            autoclose: true,
-            todayBtn: true,
-            pickerPosition: "bottom-left"
-            });
-          $(document).ready(function(){
-          $('#id_applied_water').attr('placeholder',"m³");
-        });
-      </script>
-{% endblock %}

aira/templates/aira/performance-chart.html
@@ -1,63 +1,0 @@
-{% extends 'aira/base/main.html' %}
-{% load static %}
-{% load i18n %}
-
-{% load bootstrap3 %}
-{% block title %} {{ f.name}} {% endblock %}
-
-{% block content  %}
-{% if f.chart.inside_non_irrigation_period %}
- <div class="alert alert-info">
-   <i class="fa fa-info-circle"></i>
-   {% trans "The Irrigation Performance graph is available only during irrigation period (16 March to 15 October)" %}
- </div
-{% endif %}
-<h4 style="text-align: center;">{% trans 'Irrigation Performance' %}</h4>
-{% if f.status != 'done' %}
-    <p class="alert alert-warning">{% trans "This field is being (re)calculated. Reload the page some time later to view the updated results. The calculation usually takes from a few seconds to a few minutes." %}</p>
-{% endif %}
-<div id="irrchart" style="width:100%; height:400px;"></div>
-<div class="container">
-    {% if f.chart %}
-        <a style="float:right"  href="{% url 'performance_csv' f.id %}"> <i class="fa fa-cloud-download"></i> {% trans "Download Chart Data" %}</a><br>
-        <b>{% trans "Total Effective Precipitation" %}</b>: {{ f.chart.chart_irr_period_peff_cumulative|floatformat:"1" }} mm <br>
-        <hr>
-        <b>{% trans 'Total Estimated Irrigation Water Amount' %}</b>: {{ f.chart.sum_ifinal|floatformat:"1"  }} mm <br>
-        <b>{% trans "Total Applied Irrigation Water Amount" %}</b>: {{ f.chart.sum_applied_water|floatformat:"1" }} mm <br>
-        <b>{% trans "Percentage difference"%}</b>: {{ f.chart.percentage_diff }} % <br>
-    {% endif %}
-</div>
-<br><br><br><br><br><br><br>
-{% endblock %}
-
-{% block extrajs %}
-<script type="text/javascript">
-   performance = {}; //empty js object
-   performance.title = " {{ f.name }}";
-   performance.subtitle = "{{ f.crop_type}} - " + "{{ f.irrigation_type }}";
-   performance.ifinal_title = "{% trans 'Estimated Irrigation Water Amount' %}";
-   performance.applied_water_title = "{% trans 'Applied Irrigation Water Amount' %}";
-   performance.peff_title = "{% trans 'Effective precipitation' %}"
-   performance.yAxis_title =  " ";
-   {% if f.chart %}
-        performance.dates = [
-                    {% for d in f.chart.chart_dates  %}
-                        "{{ d | safe }}" {% if not forloop.last %},{% endif %}
-                    {% endfor %}
-            ];
-        performance.ifinal = {{ f.chart.chart_ifinal }};
-        performance.applied_water = {{ f.chart.applied_water}};
-        performance.peff = {{ f.chart.chart_peff }}
-    {% else %}
-        performance.dates = [];
-        performance.ifinal = null;
-        performance.applied_water = null;
-        performance.peff = null;
-    {% endif %}
-</script>
-
-<script src="http://code.highcharts.com/highcharts.js" type="text/javascript"></script>
-<script src="http://code.highcharts.com/modules/exporting.js" type="text/javascript"></script>
-<script type="text/javascript" src="{% static 'js/performance-chart.js' %}"></script>
-
-{% endblock %}

aira/templates/aira/performance_chart/main.html
@@ -1,0 +1,63 @@
+{% extends 'aira/base/main.html' %}
+{% load static %}
+{% load i18n %}
+
+{% load bootstrap3 %}
+{% block title %} {{ f.name}} {% endblock %}
+
+{% block content  %}
+  {% if f.chart.inside_non_irrigation_period %}
+    <div class="alert alert-info">
+      <i class="fa fa-info-circle"></i>
+      {% trans "The Irrigation Performance graph is available only during irrigation period (16 March to 15 October)" %}
+    </div
+  {% endif %}
+  <h4 style="text-align: center;">{% trans 'Irrigation Performance' %}</h4>
+  {% if f.status != 'done' %}
+    <p class="alert alert-warning">{% trans "This field is being (re)calculated. Reload the page some time later to view the updated results. The calculation usually takes from a few seconds to a few minutes." %}</p>
+  {% endif %}
+  <div id="irrchart" style="width:100%; height:400px;"></div>
+  <div class="container">
+    {% if f.chart %}
+      <a style="float:right"  href="{% url 'performance_csv' f.id %}"> <i class="fa fa-cloud-download"></i> {% trans "Download Chart Data" %}</a><br>
+      <b>{% trans "Total Effective Precipitation" %}</b>: {{ f.chart.chart_irr_period_peff_cumulative|floatformat:"1" }} mm <br>
+      <hr>
+      <b>{% trans 'Total Estimated Irrigation Water Amount' %}</b>: {{ f.chart.sum_ifinal|floatformat:"1"  }} mm <br>
+      <b>{% trans "Total Applied Irrigation Water Amount" %}</b>: {{ f.chart.sum_applied_water|floatformat:"1" }} mm <br>
+      <b>{% trans "Percentage difference"%}</b>: {{ f.chart.percentage_diff }} % <br>
+    {% endif %}
+  </div>
+  <br><br><br><br><br><br><br>
+{% endblock %}
+
+{% block extrajs %}
+  <script type="text/javascript">
+    performance = {}; //empty js object
+    performance.title = " {{ f.name }}";
+    performance.subtitle = "{{ f.crop_type}} - " + "{{ f.irrigation_type }}";
+    performance.ifinal_title = "{% trans 'Estimated Irrigation Water Amount' %}";
+    performance.applied_water_title = "{% trans 'Applied Irrigation Water Amount' %}";
+    performance.peff_title = "{% trans 'Effective precipitation' %}"
+    performance.yAxis_title =  " ";
+    {% if f.chart %}
+      performance.dates = [
+        {% for d in f.chart.chart_dates  %}
+          "{{ d | safe }}" {% if not forloop.last %},{% endif %}
+        {% endfor %}
+      ];
+      performance.ifinal = {{ f.chart.chart_ifinal }};
+      performance.applied_water = {{ f.chart.applied_water}};
+      performance.peff = {{ f.chart.chart_peff }}
+    {% else %}
+      performance.dates = [];
+      performance.ifinal = null;
+      performance.applied_water = null;
+      performance.peff = null;
+    {% endif %}
+  </script>
+
+  <script src="http://code.highcharts.com/highcharts.js" type="text/javascript"></script>
+  <script src="http://code.highcharts.com/modules/exporting.js" type="text/javascript"></script>
+  <script type="text/javascript" src="{% static 'js/performance-chart.js' %}"></script>
+
+{% endblock %}

aira/templates/aira/profile_confirm_delete.html
@@ -1,30 +1,0 @@
-{% extends 'aira/base/main.html' %}
-{% load i18n %}
-
-{% load bootstrap3 %}
-
-{% block title %} {{ user }} {% endblock %}
-
-{% block content %}
-<div class="container">
-  <div class="row">
-      <div class="col-md-12 col-xs-offset-2">
-         <h3>{% trans "Are you sure you want to delete all user data" %}
-           {% if LANGUAGE_CODE = 'el' %}
-            <br>
-           {% endif %}
-           "<a href="{% url 'update_profile' object.id %}">{{ object.farmer }}</a>" {% trans "?" %}
-         </h3>
-         <br>
-          <form method="post" role="form" class="form-horizontal">
-            {% csrf_token %}
-            {% buttons %}
-            <div class="col-sm-offset-3 col-sm-8">
-                <button type="submit" class="btn btn-danger">{% trans "Delete" %}</button>
-            </div>
-            {% endbuttons %}
-          </form>
-        </div>
-  </div>
-</div>
-{% endblock %}

aira/templates/aira/profile_form.html
@@ -1,49 +1,0 @@
-{% extends 'aira/base/main.html' %}
-{% load i18n %}
-
-{% load bootstrap3 %}
-{% block title %} {% trans "Account Settings" %} {% endblock %}
-
-{% block content %}
-{% if user.username == "demo" %}
- <div class="alert alert-info">
-    <a href="#" class="close" data-dismiss="alert">&times;</a>
-    <i class="fa fa-info-circle fa-2x"></i>&nbsp; <span style="font-size:32px">{% trans "demo" %}</span>
-</div>
-{% endif %}
-  <form method="post" role="form" >
-    {% csrf_token %}
-    <div class='row'>
-          <div class='col-sm-5'>
-          <h4 class="lead">{% trans "Update" %} {% trans "Profile" %}</h4>
-            <div class='form-group'>
-              {% bootstrap_field form.first_name %}
-              {% bootstrap_field form.last_name %}
-              {% bootstrap_field form.address %}
-              {% bootstrap_field form.supervision_question %}
-              {% if user.username != "demo" %}
-                    <button type="submit" class="btn  btn-sm btn-primary">{% trans "Update" %}</button>
-                    <a class="btn btn-sm btn-primary"href="{% url 'auth_password_reset' %}">{% trans "Reset password" %}</a>
-                {% if user.profile.id %}
-                  <a class="btn btn-sm btn-danger" href="{% url 'delete_profile' user.profile.id %}"> {% trans "Delete account" %}</a>
-                {% endif %}
-              {% endif %}
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <h4 class="lead">{% trans "Notification Setup" %}</h4>
-             <div class="form-group">
-                {% bootstrap_field form.notification %}
-                {% bootstrap_field form.email_language %}
-             </div>
-          </div>
-          <div class="col-sm-3">
-            <h4 class="lead">{% trans "Supervision" %}</h4>
-             <div class="form-group">
-                {% bootstrap_field form.supervisor %}
-             </div>
-          </div>
-    </div>
-
-  </form>
-{% endblock%}

commit 18ba86394be355ab7270b188b19fc059e4dd9611

Fix profile update/delete views (fixes #243)
This commit fixes the update/delete views, which had been broken since
e02225e, when I had deleted the templates, thinking they weren't being
used.

In this commit I'm also deleting the CreateProfileView, which doesn't
appear to be used.

aira/templates/aira/profile/confirm_delete.html
@@ -1,0 +1,29 @@
+{% extends 'aira/base/main.html' %}
+{% load i18n %}
+
+{% load bootstrap3 %}
+
+{% block title %} {{ user }} {% endblock %}
+
+{% block content %}
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h3>
+        {% blocktrans with username=object.user.username first_name=object.first_name last_name=object.last_name %}
+          Are you sure you want to delete user "{{ username }}"
+          ({{first_name}} {{last_name}}) and all his data?
+        {% endblocktrans %}
+      </h3>
+      <form method="post" role="form" class="form-horizontal">
+        {% csrf_token %}
+        {% buttons %}
+        <div class="col-sm-offset-3 col-sm-8">
+          <button type="submit" class="btn btn-danger">{% trans "Delete" %}</button>
+        </div>
+        {% endbuttons %}
+      </form>
+    </div>
+  </div>
+</div>
+{% endblock %}

aira/templates/aira/profile/form.html
@@ -1,0 +1,50 @@
+{% extends 'aira/base/main.html' %}
+{% load i18n %}
+{% load bootstrap3 %}
+
+
+{% block title %}{% trans "Account Settings" %}{% endblock %}
+
+
+{% block content %}
+{% if user.username == "demo" %}
+ <div class="alert alert-info">
+    <a href="#" class="close" data-dismiss="alert">&times;</a>
+    <i class="fa fa-info-circle fa-2x"></i>&nbsp; <span style="font-size:32px">{% trans "demo" %}</span>
+</div>
+{% endif %}
+  <form method="post" role="form" >
+    {% csrf_token %}
+    <div class='row'>
+      <div class='col-sm-5'>
+        <h4 class="lead">{% trans "Update" %} {% trans "Profile" %}</h4>
+        <div class='form-group'>
+          {% bootstrap_field form.first_name %}
+          {% bootstrap_field form.last_name %}
+          {% bootstrap_field form.address %}
+          {% bootstrap_field form.supervision_question %}
+          {% if user.username != "demo" %}
+            <button type="submit" class="btn  btn-sm btn-primary">{% trans "Update" %}</button>
+            <a class="btn btn-sm btn-primary"href="{% url 'auth_password_reset' %}">{% trans "Reset password" %}</a>
+            {% if user.profile.id %}
+              <a class="btn btn-sm btn-danger" href="{% url 'delete_profile' user.profile.id %}"> {% trans "Delete account" %}</a>
+            {% endif %}
+          {% endif %}
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <h4 class="lead">{% trans "Notification Setup" %}</h4>
+        <div class="form-group">
+          {% bootstrap_field form.notification %}
+          {% bootstrap_field form.email_language %}
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <h4 class="lead">{% trans "Supervision" %}</h4>
+        <div class="form-group">
+          {% bootstrap_field form.supervisor %}
+        </div>
+      </div>
+    </div>
+  </form>
+{% endblock%}

aira/urls.py
@@ -25,11 +25,6 @@ urlpatterns = [
     ),
     # Profile
     path(
-        "create_profile/",
-        login_required(views.CreateProfileView.as_view()),
-        name="create_profile",
-    ),
-    path(
         "update_profile/<int:pk>/",
         login_required(views.UpdateProfileView.as_view()),
         name="update_profile",

aira/views.py
@@ -155,29 +155,11 @@ class RecommendationView(DetailView):
         return context
 
 
-# Profile Create/Update
-class CreateProfileView(CreateView):
-    model = Profile
-    form_class = ProfileForm
-    success_url = "/home"
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-        if self.request.user in form.fields["supervisor"].queryset:
-            form.fields["supervisor"].queryset = form.fields[
-                "supervisor"
-            ].queryset.exclude(pk=self.request.user.id)
-        return form
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-
 class UpdateProfileView(UpdateView):
     model = Profile
     form_class = ProfileForm
     success_url = "/home"
+    template_name_suffix = "/form"
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
@@ -197,11 +179,11 @@ class UpdateProfileView(UpdateView):
 
 class DeleteProfileView(DeleteView):
     model = Profile
+    template_name_suffix = "/confirm_delete"
 
     def get_success_url(self):
         profile = Profile.objects.get(pk=self.kwargs["pk"])
         user = User.objects.get(pk=profile.user.id)
-        # Delete all user data using bult in cascade delete
         user.delete()
         return reverse("welcome")
 

aira/tests/test_views.py
@@ -498,3 +498,25 @@ class RemoveSupervisedUserTestCase(TestDataMixin, TestCase):
         self.client.login(username="alice", password="topsecret")
         response = self.client.post("/supervised_user/remove/")
         self.assertEqual(response.status_code, 404)
+
+
+class ProfileViewsTestCase(TestCase):
+    def setUp(self):
+        self.bob = User.objects.create_user(username="bob", password="topsecret")
+        self.bob.profile.first_name = "Bob"
+        self.bob.profile.last_name = "Brown"
+        self.bob.profile.save()
+        self.client.login(username="bob", password="topsecret")
+
+    def test_get_update_view(self):
+        response = self.client.get("/update_profile/{}/".format(self.bob.id))
+        self.assertContains(response, "Bob")
+
+    def test_get_delete_confirmation(self):
+        response = self.client.get("/delete_profile/{}/".format(self.bob.id))
+        self.assertContains(response, "Bob")
+
+    def test_confirm_delete(self):
+        response = self.client.post("/delete_profile/{}/".format(self.bob.id))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(User.objects.filter(username="bob").exists())